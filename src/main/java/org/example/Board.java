65c927aedb68f1813a30cfd36ebe962db7258dee
Sarthak Rohatgi <sarthak.rohatgi_ftc@external.swiggy.in>
2024-02-08 01:41:29 +0530

    cells logic transferred from board to cells

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -7,62 +7,36 @@ import org.example.neighborcalculationservice.DefaultNeighborCalculationService;
 import org.example.neighborcalculationservice.NeighborCalculationService;
 import org.example.outputservice.OutputService;
 
-import java.util.Random;
-
 public class Board {
-    Cell[][] grid;
+    Cells cells;
     NeighborCalculationService neighborCalculationService;
 
     Board(int m, int n, int seedPercentage) {
         if(m <= 0 || n <= 0 )  {
             throw new IllegalArgumentException("M, N or Seed Percentage cannot be equal or less than 0");
         }
-        this.grid = new Cell[m][n];
-        initializeBoardCellsDead();
-        setInitialSeeds(m, n, seedPercentage);
+
+        this.cells = new Cells(m, n, seedPercentage);
         this.neighborCalculationService = new DefaultNeighborCalculationService();
     }
 
-    private void initializeBoardCellsDead() {
-        for(int i = 0; i < grid.length; i++) {
-            for(int j = 0; j < grid[0].length; j++) {
-                this.grid[i][j] = new DeadCell();
-            }
-        }
-    }
-
-    private void setInitialSeeds(int m, int n, int seedPercentage) {
-        Random random = new Random();
-        int totalCells = m*n;
-        double seedPerc = seedPercentage * 0.01;
-        int cellsToSeed = (int)(totalCells * seedPerc);
-
-        while(cellsToSeed > 0) {
-            int randomRow = random.nextInt(m);
-            int randomCol = random.nextInt(n);
-            if(this.grid[randomRow][randomCol].getClass() == AliveCell.class) continue;
-            this.grid[randomRow][randomCol] = new AliveCell();
-            cellsToSeed--;
-        }
-    }
-
     public Cell getCell(int row, int col) {
-        if (row >= 0 && row < grid.length && col >= 0 && col < grid[0].length) {
-            return grid[row][col];
+        if (row >= 0 && row < cells.grid.length && col >= 0 && col < cells.grid[0].length) {
+            return cells.grid[row][col];
         }
         return new DeadCell();
     }
 
-    private void nextGen() {
-        Cell[][] newGrid = new Cell[this.grid.length][this.grid[0].length];
-        for(int i = 0; i < grid.length; i++) {
-            for(int j = 0; j < grid[0].length; j++) {
+    protected void nextGen() {
+        Cell[][] newGrid = new Cell[this.cells.grid.length][this.cells.grid[0].length];
+        for(int i = 0; i < cells.grid.length; i++) {
+            for(int j = 0; j < cells.grid[0].length; j++) {
                 int liveNeighbors = this.neighborCalculationService.calculateNeighbors(this, i, j);
-                newGrid[i][j] = this.grid[i][j].nextState(liveNeighbors);
+                newGrid[i][j] = this.cells.grid[i][j].nextState(liveNeighbors);
             }
         }
 
-        this.grid = newGrid;
+        this.cells.updateCells(newGrid);
     }
 
     void evolve() throws InterruptedException {
@@ -70,7 +44,7 @@ public class Board {
         int tick = 1;
         while(true) {
             outputService.printGeneration(tick);
-            outputService.print(this, grid.length, grid[0].length);
+            outputService.print(this, cells.grid.length, cells.grid[0].length);
             nextGen();
             Thread.sleep(1000);
             tick++;
